613301bd65d41b1f0ac1067869dfcc31fbf724b7
Johan Svensson <johan.svensson93@hotmail.se>
2021-08-20 10:38:32 +0200

    Add the rest of the test.

---
+++ Character.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Preparatory_Test
+{
+    public class Character
+    {
+        public string Name { get; set; }
+        public int Health { get; set; }
+        public int Strength { get; set; }
+        public int Luck { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,8 +10,7 @@ namespace Preparatory_Test
 
         static void Main(string[] args)
         {
-            Program p = new Program();
-            p.IsPalindrome();
+
         }
 
         // Returns Hello World to user
@@ -203,17 +202,129 @@ namespace Preparatory_Test
             return false;
         }
 
+        // 13. Function that takes two inputs and returns the values between the two inputs. Starting from the lowest one
         public void ReturnInputNumbers()
         {
             Console.WriteLine("Type a number");
             var inputOne = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Type a new number");
+            Console.WriteLine("Type a second number");
             var inputTwo = Convert.ToInt32(Console.ReadLine());
+            int difference;
 
-            // Skriv ut alla siffror mellan input 1 och 2.
+            // Checks if input 1 is higher than input 1
+            // To return value from lowest number
+            if (inputOne > inputTwo)
+            {
+                Console.WriteLine($"Numbers between {inputTwo} and {inputOne}");
+
+                // Checks the difference between the inputs
+                // to use as loop condition
+                difference = inputOne - inputTwo;
+
+                for (int i = 0; i < difference; i++)
+                {
+                    inputTwo++;
+                    // Writes an increment of input 2 
+                    // because it's the lowest number
+                    Console.Write($"{inputTwo}\t");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Numbers between {inputOne} and {inputTwo}");
+
+                // Checks the difference between the inputs
+                // to use as loop condition
+                difference = inputTwo - inputOne;
+
+                for (int i = 0; i < difference; i++)
+                {
+                    inputOne++;
+                    // Writes an increment of input 2 
+                    // because it's the lowest number
+                    Console.Write($"{inputOne}\t");
+                }
+            }
+        }
+
+        // 14. Splits and returns the input value between odd and even numbers
+        public void SortOddAndEvenNumbers()
+        {
+            Console.WriteLine("Enter a few numbers separated by comma");
+            var input = Console.ReadLine();
+
+            // Splits the input at commas
+            var inputSplitted = input.Split(',');
+
+            Console.WriteLine("Even Numbers");
+
+            // Loop through the numbers
+            foreach (var evenNumber in inputSplitted)
+            {
+                // Check if the number is divided by 2
+                if (Convert.ToInt32(evenNumber) % 2 == 0)
+                    // Writes the even numbers
+                    Console.Write(evenNumber.Trim() + "\t");
+            }
+
+            Console.WriteLine("\nOdd numbers");
+            foreach (var oddNumber in inputSplitted)
+            {
+                // Check if the number is not divided by 2
+                if (Convert.ToInt32(oddNumber) % 2 == 1)
+                    // Writes the odd numbers
+                    Console.Write(oddNumber.Trim() + "\t");
+            }
+
+            Console.Read();
 
         }
 
+        // 15. Adds the user inputs and returns the sum
+        public void AddInputValues()
+        {
+            Console.WriteLine("Enter a few numbers separated by comma");
+            var input = Console.ReadLine();
+            // Splits the input at commas and trims it from whitespaces
+            var inputSplitted = input.Trim().Split(',');
+            
+            // Converts the input array to int array
+            var intArray = Array.ConvertAll(inputSplitted, int.Parse);
 
+            // Writes the sum of the int array
+            Console.WriteLine(intArray.Sum());
+            Console.Read();
+
+        }
+
+        // 16. User sets the name of his and the enemy character. Health, strength and luck is random generated.
+        public void SetCharacterNames()
+        {
+            // User and Enemy character instances
+            var userCharacter = new Character();
+            var enemyCharacter = new Character();
+
+            Random rnd = new Random();
+            int minValue = 1;
+            int maxValue = 101;
+
+            Console.WriteLine("Enter a name for your character");
+            // Sets the name for the user character
+            userCharacter.Name = Console.ReadLine();
+            Console.WriteLine("Enter a name for the enemy character");
+            // Sets the name for the enemy character
+            enemyCharacter.Name = Console.ReadLine();
+
+            // Random generated values for enemy health, strength and luck
+            enemyCharacter.Health = rnd.Next(minValue, maxValue);
+            enemyCharacter.Strength = rnd.Next(minValue, maxValue);
+            enemyCharacter.Luck = rnd.Next(minValue, maxValue);
+
+            // Random generated values for user health, strength and luck
+            userCharacter.Health = rnd.Next(minValue, maxValue);
+            userCharacter.Strength = rnd.Next(minValue, maxValue);
+            userCharacter.Luck = rnd.Next(minValue, maxValue);
+
+        }
     }
 }
